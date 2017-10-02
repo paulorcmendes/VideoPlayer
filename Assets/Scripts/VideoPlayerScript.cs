ea7d0f9a5bf058cf815ac58d965692a25fbc0c65
paulorcmendes <mendesdpn@gmail.com>
2017-10-02 15:40:00 -0300

    initial commit

---
+++ Assets/Scripts/VideoPlayerScript.cs
@@ -0,0 +1,22 @@
+﻿// Examples of VideoPlayer function
+
+using UnityEngine;
+
+public class VideoPlayerScript : MonoBehaviour
+{
+    void Start()
+    {
+   
+        GameObject camera = GameObject.Find("Main Camera");
+
+        var videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
+        var audio = videoPlayer.GetTargetAudioSource(0);
+        videoPlayer.Play();
+        audio.Play();
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        vp.playbackSpeed = vp.playbackSpeed / 10.0F;
+    }
+}
